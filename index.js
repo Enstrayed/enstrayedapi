96a8290a0e41d99a5aa386c0bace766116c44e4f
Enstrayed <48845980+Enstrayed@users.noreply.github.com>
2024-03-27 16:47:45 -0700

    Remove Redis as dependency

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const fs = require('fs');         // Filesystem Access
 const express = require('express'); 
 const app = express();            // Init Express
-const Redis = require('ioredis'); // Init Redis
 
 const globalConfig = JSON.parse(fs.readFileSync('config.json', 'utf-8')) // Read config file
 
-const db = new Redis({
-    host: globalConfig.redisServer,    
-    port: globalConfig.redisPort
-})
-module.exports = { app, db, globalConfig } // Export database connection for other files
+module.exports = { app, globalConfig, fs } // Export express app and fs objects and globalconfig
 
 app.use(express.json()) // Allows receiving JSON bodies
 // see important note: https://expressjs.com/en/api.html#express.json
 
-fs.readdir(globalConfig.routesDirectory, (err, files) => {
+// Import Routes
+fs.readdir(globalConfig.startup.routesDir, (err, files) => {
     if (err) {
-        console.log("FATAL: Unable to read ./routes")
+        console.log(`FATAL: Unable to read ${globalConfig.startup.routesDir}`)
+        process.exit(1)
     } else {
         let importedRoutes = []
         files.forEach(file => {
-            importedRoutes.push(file)
-            require(`${globalConfig.routesDirectory}/${file}`)
+            require(`${globalConfig.startup.routesDir}/${file}`)
+            importedRoutes.push(file.slice(0,-3))
         })
         console.log(`Imported Routes: ${importedRoutes}`)
     }
 })
 
-console.log(`Started on ${globalConfig.apiPort}`)
-app.listen(globalConfig.apiPort);``
+app.get("/", (rreq,rres) => {
+    rres.redirect(globalConfig.startup.documentationUrl)
+})
+
+console.log(`Started on ${globalConfig.startup.apiPort}`)
+app.listen(globalConfig.startup.apiPort)
